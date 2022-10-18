916a5097ea6b95647c14639e45cd035efebc6448
Felipe Reis <111073491+felipereis068@users.noreply.github.com>
2022-10-18 14:10:33 -0500

    editar

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+from wsgiref.util import request_uri
+from MySQLdb import DataError
 from django.shortcuts import redirect, render
 from app.forms import CarrosForm
 from app.models import Carros
@@ -23,7 +25,20 @@ def view(request, pk):
     data = {}
     data['crud'] = Carros.objects.get(pk=pk)  
     return render(request, 'view.html', data)
-    
+
+def edit(request, pk):
+    data = {}
+    data['crud'] = Carros.objects.get(pk=pk)
+    data['form'] = CarrosForm(instance=data['crud'])
+    return render(request, 'form.html', data)
+
+def update(request, pk):
+    data = {}
+    data['crud'] = Carros.objects.get(pk=pk)
+    form = CarrosForm(request.POST or None, instance=data['crud'])
+    if form.is_valid():
+        form.save()
+        return redirect('home')
 
 
 
